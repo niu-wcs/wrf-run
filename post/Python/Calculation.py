09f2499260c0f8d0828542568ca331c06e60e7c1
Robert Fritzen <rfritzen@phantomdev.net>
2019-10-13 13:57:55 -0500

    Fix missing parameters

--- post/Python/Calculation.py
+++ post/Python/Calculation.py
@@ -171,12 +171,12 @@ def pvo_wrap(u, v, full_t, full_p, msfu, msfv, msfm, cor, dx, dy, omp_threads=1)
 	This block of code handles the multiprocessed variable calculation routines.
 	 -> These are wrapped calls of the original g_func* methods in the wrf-python library
 """
-def get_full_p(daskArray):
+def get_full_p(daskArray, omp_threads=1):
 	p = fetch_variable(daskArray, "P")
 	pb = fetch_variable(daskArray, "PB")
 	
-	total_p = map_blocks(wrapped_add, p, pb, dtype=p.dtype)
-	full_p = map_blocks(wrapped_div, total_p, 100, dtype=p.dtype)
+	total_p = map_blocks(wrapped_add, p, pb, omp_threads, dtype=p.dtype)
+	full_p = map_blocks(wrapped_div, total_p, 100, omp_threads, dtype=p.dtype)
 
 	return full_p
 
